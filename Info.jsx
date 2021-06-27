683bb3c2e5be81a36466af67ec723a067b04da9a
Jeiranjeni <85471680+Jeiranjeni@users.noreply.github.com>
2021-06-27 02:46:40 -0500

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript And React.js</h1>
+      <p>Basic Web-development with react.js bootcamps</p>
+      <p>
+        This was an amazing 7 days Bootcamp taken by top MNC interns for free
+      </p>
+      <p>Surely you can get knowledge to create websites using react.js</p>
+    </div>
+  );
+}
+
+export default Info;
